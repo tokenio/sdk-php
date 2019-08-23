9a0243049681af0be8193401d057c8eea055cb5d
Max Kucherenko <>
2019-08-23 16:44:34 +0300

    Build script now fails if PHP doc is incorrect or contais errors

--- build.sh
+++ build.sh
@@ -13,8 +13,15 @@ if ./vendor/bin/phpunit --testdox ./tests/; then
         rm -rf ./doc/generated
         mkdir ./doc/generated
         echo 'Generate Documentation'
-        php ./tools/sami.phar update ./doc/config.php
-        rm -rf ./cache
+        if php ./tools/sami.phar -v update ./doc/config.php; then
+            echo "$(tput setaf 2)Documentation Generated Successfully$(tput sgr 0)"
+            rm -rf ./cache
+        else
+            echo "$(tput setaf 1)Documentation Generation Failed$(tput sgr 0)"
+            rm -rf ./doc/generated
+            rm -rf ./cache
+            exit 1
+        fi
 else
         exit 1
 fi
