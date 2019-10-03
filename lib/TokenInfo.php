ab833af66fa20681c65d18f97fe60c2949bfdeec
Gianluca Pane <31416110+gianluca-token@users.noreply.github.com>
2019-10-03 16:10:51 -0700

    RD-3416: set sdk version header based on value in composer.json (#23)

--- lib/TokenInfo.php
+++ lib/TokenInfo.php
@@ -6,9 +6,7 @@ class TokenInfo
 {
     const TOKEN_SDK = 'token-sdk';
     const TOKEN_SDK_VALUE = 'php';
-
     const TOKEN_SDK_VERSION = 'token-sdk-version';
-    const TOKEN_SDK_VERSION_VALUE = '1.0.0';
 
     const TOKEN_DEV_KEY = 'token-dev-key';
 }
